bfc148dd4606c4aa7386c25a71263b4807740df2
Anderson Silva <andeerson0017@gmail.com>
2020-10-23 16:43:34 -0300

    :boom: chore: Lazy load de componentes

---
+++ src/components/AddToCartModal.tsx
@@ -0,0 +1,7 @@
+export default function AddToCartModal (){
+  return (
+    <div>
+      Deseja realmente adicionar esse produto ao carrinho ?
+    </div>
+  )
+}
--- src/pages/catalog/product/[slug].tsx
+++ src/pages/catalog/product/[slug].tsx
@@ -1,9 +1,30 @@
 import { useRouter } from 'next/router'
 import { Title } from '../../../styles/pages/Home'
+import dynamc from 'next/dynamic'
+import { useState } from 'react'
+
+const AddToCartModal = dynamc(
+  () => import('../../../components/AddToCartModal'),
+  {loading: () => <p>Carregando...</p>}
+ )
 
 export default function Produto(){
 
   const router = useRouter()
 
-  return <Title>{router.query.slug}</Title>
+  const [isAddToCartModalVisible, setIsAddToCartModalVisible] = useState(false)
+
+  const handleAddToCart = () => {
+    setIsAddToCartModalVisible(true)
+  }
+
+  return (
+    <div>
+      <Title>{router.query.slug}</Title>
+
+      <button onClick={handleAddToCart}>Add to Cart</button>
+
+      {isAddToCartModalVisible && <AddToCartModal/>}
+    </div>
+  )
 }
